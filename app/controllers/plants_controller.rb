fbb45b6710db894449363949ef0c492bd33a4d5b
Nancy Zaky <nancyzki@yahoo.com>
2021-09-30 18:15:12 -0700

    commit tree 597a4fbc42bdecd15ab9ed1f7264167f21e68eaf

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,4 +1,5 @@
 class PlantsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   # GET /plants
   def index
@@ -17,10 +18,22 @@ class PlantsController < ApplicationController
     plant = Plant.create(plant_params)
     render json: plant, status: :created
   end
-
+  def update
+  plant = Plant.find_by(id:params[:id])
+  plant.update(plant_params)
+  render json: plant
+  end
+  def destroy
+ plant = Plant.find_by(id: params[:id])
+  plant.destroy
+  head :no_content
+  end
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+  def render_not_found_response
+    render json: {error: "plant not found"}, status: :not_found
   end
 end
